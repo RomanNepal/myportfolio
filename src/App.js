ab1ab350d85bcfe1bde5ae8beb0fd4ecf7b076f3
Roman <nepalroman100@gmail.com>
2024-02-08 19:21:50 +0545

    trying to fix

--- src/App.js
+++ src/App.js
@@ -71,97 +71,96 @@ function App() {
     setTheme(theme === "dark" ? "light" : "dark");
   };
   useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
-      // await loadSlim(engine);
-      await loadBasic(engine);
-    }).then(() => {
-      setInit(true);
-    });
+    if (!init) {
+      initParticlesEngine(async (engine) => {
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadAll(engine);
+        //await loadFull(engine);
+        // await loadSlim(engine);
+        await loadBasic(engine);
+      }).then(() => {
+        setInit(true);
+      });
+    }
   }, []);
   return (
     <>
-      {init ? (
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: "gray",
-              },
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "gray",
             },
-            fpsLimit: 60,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
                 enable: true,
-                opacity: 0.5,
-                width: 1,
+                mode: "push",
               },
-              move: {
-                direction: "none",
+              onHover: {
                 enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 2,
-                straight: false,
+                mode: "repulse",
               },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 50,
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
               },
-              opacity: {
-                value: 0.1,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
               },
             },
-            detectRetina: true,
-          }}
-        />
-      ) : (
-        ""
-      )}
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.1,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: false,
+        }}
+      />
+
       <div className="bg-white dark:bg-[#1B1B1B] transition duration-500 ease-in-out flex flex-col font-inter">
         <div
           className="flex rounded-[20px] fixed right-24 top-4"
