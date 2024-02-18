080f966864d33843f4e2e1cbdeff6bab7ca89996
Roman <nepalroman100@gmail.com>
2024-02-18 16:36:50 +0545

    folder name capitalized

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,3 @@
 import { configureStore } from "@reduxjs/toolkit";
-import colorReducer from "../features/color/colorSlice";
+import colorReducer from "../Features/Color/colorSlice";
 export const store = configureStore({ reducer: { color: colorReducer } });
